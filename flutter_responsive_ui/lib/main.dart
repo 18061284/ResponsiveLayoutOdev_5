548679a41fcd9812cfcc2e91e0025f722f287dca
18061284 <18061284@stu.omu.edu.tr>
2022-11-06 11:56:19 +0300

    new

---
+++ flutter_responsive_ui/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_responsive_ui/LoginExample.dart';
+import 'package:flutter_responsive_ui/MediaQueryExample.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: MediaQueryExample(),
+));
